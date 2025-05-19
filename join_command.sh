91257394f7f27862bc6d3678df6f687e3f4c221f
Razvan Loghin <R.Loghin@student.tudelft.nl>
2025-05-19 20:58:23 +0200

    Install Istio and VM boots up properly

--- join_command.sh
+++ join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.100:6443 --token luvddw.xyrffinftfjkgs2y --discovery-token-ca-cert-hash sha256:b1f0cfecee8f9cb2859342f41e1e81c7f77dee885edfcadaaef7afb542ca45c5 --cri-socket=unix:///run/containerd/containerd.sock
+kubeadm join 192.168.56.100:6443 --token ixl3a6.ym6b1yuiv9uffxji --discovery-token-ca-cert-hash sha256:011f40da156ef2af6efa26cbcc359e6741dff2ec04daef907da5dc92283a5c16 --cri-socket=unix:///run/containerd/containerd.sock
