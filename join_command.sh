f855c68977ecd5e7bbcd50e554e01a61a23a33aa
cristiancomend <C.Comendant-1@student.tudelft.nl>
2025-06-22 09:18:15 +0200

    Add real secret

--- join_command.sh
+++ join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.100:6443 --token y4s5fo.eporzq1ri1p638rt --discovery-token-ca-cert-hash sha256:57ecc43b393fad26aad8f420bae05a8dd43328bacce8e74e71629f9af8ed7df5 --cri-socket=unix:///run/containerd/containerd.sock
+kubeadm join 192.168.56.100:6443 --token 094pjd.blgg0unupfqgpsho --discovery-token-ca-cert-hash sha256:11a54b21b375280a9042b68901153bdbc662a102fac9a865f5d2ed4383270f4c --cri-socket=unix:///run/containerd/containerd.sock
